84bc68c8d3607c75bf14302b3b0e568138c4b031
Floyd Huizinga <floyd@engine-software.nl>
2024-12-25 21:08:50 +0100

    more timer fixes

--- timer.h
+++ timer.h
@@ -4,11 +4,12 @@
 class Timer
 {
 public:
-	void Start(double durationSeconds)
+	void Start(double durationSeconds, double remainderLeeway = 0.0333)
 	{
 		start = std::chrono::high_resolution_clock::now();
-		this->duration = durationSeconds;
+		duration = durationSeconds;
 		remainder = 0;
+		this->remainderLeeway = remainderLeeway;
 	}
 
 	bool HasFinished()
@@ -26,24 +27,27 @@ public:
 	void RestartWithRemainder()
 	{
 		remainder = GetTimeLeft();
+		if (remainder < -remainderLeeway)
+		{
+			remainder = -remainderLeeway;
+		}
 		start = std::chrono::high_resolution_clock::now();
 	}
 
 	double GetProgress()
 	{
 		std::chrono::duration<double>  delta = std::chrono::high_resolution_clock::now() - start;
-		return std::min((delta.count() ) / duration, 1.0);
+		return std::min((delta.count() + remainder) / duration, 1.0);
 	}
 	
 	double GetTimeLeft()
 	{
 		std::chrono::duration<double>  delta = std::chrono::high_resolution_clock::now() - start;
-		return duration - (delta.count());
+		return duration - (delta.count() + remainder);
 	}
 private:
-	constexpr static long long ToSec = 1'000'000'000'000;
 	std::chrono::high_resolution_clock::time_point start;
 	double remainder;
 	double duration;
-
+	double remainderLeeway;
 };
