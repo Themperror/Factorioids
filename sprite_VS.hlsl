111531c5008643131434109bc0cf76c9bd057909
Floyd Huizinga <floyd@engine-software.nl>
2024-12-25 21:09:10 +0100

    fix mirrored sprite and Y uv issues

--- sprite_VS.hlsl
+++ sprite_VS.hlsl
@@ -18,24 +18,24 @@ VS_OUT main(float4 pos : POS, uint vertexID : SV_VertexID)
 	vertexID = vertexID % 6;
 	
 	float spriteWidth = 1.0 / (float) spriteData.numX;
-	float spriteHeight = 1.0 / (float) spriteData.numY;
+	float spriteHeight = 1.0 / (float) (spriteData.numY);
 	float spriteIndexY = (uint) pos.w / spriteData.numX;
 	float spriteIndexX = ((uint) pos.w) % spriteData.numX;
 	if (vertexID == 0)
 	{
-		vsOut.uv = float2((spriteIndexX + 1.0) * spriteWidth, spriteIndexY * spriteWidth);
+		vsOut.uv = float2(spriteIndexX * spriteWidth, spriteIndexY * spriteHeight);
 	}
 	else if (vertexID == 1 || vertexID == 3)
 	{
-		vsOut.uv = float2(spriteIndexX * spriteWidth, spriteIndexY * spriteWidth);
+		vsOut.uv = float2((spriteIndexX + 1.0) * spriteWidth, spriteIndexY * spriteHeight);
 	}
 	else if (vertexID == 2 || vertexID == 5)
 	{
-		vsOut.uv = float2((spriteIndexX + 1.0) * spriteWidth, (spriteIndexY + 1.0) * spriteWidth);
+		vsOut.uv = float2(spriteIndexX * spriteWidth, (spriteIndexY + 1.0) * spriteHeight);
 	}
 	else
 	{
-		vsOut.uv = float2(spriteIndexX * spriteWidth, (spriteIndexY + 1.0) * spriteHeight);
+		vsOut.uv = float2((spriteIndexX + 1.0) * spriteWidth, (spriteIndexY + 1.0) * spriteHeight);
 	}
 
 	return vsOut;
